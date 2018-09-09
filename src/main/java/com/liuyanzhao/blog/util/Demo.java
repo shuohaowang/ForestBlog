367c4e0e3f059be5f09fced5e741faae80ef99da
aaron <shuohao.wang@outlook.com>
2018-09-09 21:11:55 +0800

    first init

---
+++ src/main/java/com/liuyanzhao/blog/util/Demo.java
@@ -0,0 +1,14 @@
+package com.liuyanzhao.blog.util;
+
+import org.junit.jupiter.api.Test;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println(800.0 / 30 * 18);
+    }
+
+    @Test
+    public void test(){
+    }
+}
